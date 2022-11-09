9cac7344d2f6b1d1d187fdae3c0fdaa7dbf25f47
David <raymondinf23@gmail.com>
2022-11-09 01:39:25 -0400

    Remove unused import

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import Constants from "expo-constants";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
